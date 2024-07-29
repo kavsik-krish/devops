270697c8a60247f77ba4159e68a05b4c0a462a47
terraAI <terraAI@example.com>
2024-07-29 10:59:14 +0000

    Commit made by the backend process

---
+++ main.tf
@@ -0,0 +1,28 @@
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("keys.json")
+  project     = "abc"
+}
+
+resource "google_container_cluster" "default" {
+  name     = "default-cluster"
+  location = "us-central1"
+  initial_node_count = 3
+  node_config {
+    machine_type = "e2-medium"
+  }
+  master_auth {
+    username = "admin"
+  }
+  network = "default"
+  subnetwork = "projects/gcp-project-id/regions/us-central1/subnetworks/default"
+}
